3e1d6fda83be0b08083d2c953bc4431ef16c4b5d
Eric Nguyen <42782243+GotamaHuy@users.noreply.github.com>
2019-12-16 23:59:41 -0700

    added SPA

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,7 +54,12 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*'
+      resource '*', headers: :any, methods: [:get, :post, :options, :put, :delete]
+    end
+  end
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
